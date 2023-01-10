1280b17ec08ccb401de50ddcf55bd06ccc7640b2
NVD-NVD <datnguyen2847@gmail.com>
2023-01-10 15:48:15 +0700

    Update Writer Controller

--- src/main/java/com/ute/newspaper/controllers/WriterController.java
+++ src/main/java/com/ute/newspaper/controllers/WriterController.java
@@ -1,21 +1,28 @@
 package com.ute.newspaper.controllers;
 
 import com.ute.newspaper.dao.ArticleDao;
+import com.ute.newspaper.dao.TagDao;
 import com.ute.newspaper.entities.Article;
+import com.ute.newspaper.entities.Tag;
 import com.ute.newspaper.entities.User;
 import com.ute.newspaper.services.ArticleService;
 import com.ute.newspaper.utils.ServletUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @WebServlet(name = "writerServlet", value = "/writer/*")
+@MultipartConfig(fileSizeThreshold=1024*1024*10, 	// 10 MB
+        maxFileSize=1024*1024*50,      	// 50 MB
+        maxRequestSize=1024*1024*100)   	// 100 MB
 public class WriterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,8 +36,7 @@ public class WriterController extends HttpServlet {
                 List<Article> articles = ArticleDao.findAll();
                 HttpSession session = req.getSession();
                 User user = (User) session.getAttribute("authUser");
-                System.out.println("authorID: " + user.getId());
-//                List<Article> articles = ArticleService.findAllByUserID(user.getId());
+//                Lsist<Article> articles = ArticleService.findAllByUserID(user.getId());
 
                 req.setAttribute("articles", articles);
                 ServletUtils.forward("/vwWriter/pages/list.jsp", req, resp);
@@ -54,5 +60,31 @@ public class WriterController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        switch (path) {
+            case "/add":
+                add("/writer/list", req, resp);
+                break;
+
+            case "/edit":
+                ServletUtils.redirect("/writer/list", req, resp);
+                break;
+
+            default:
+                ServletUtils.forward("/404.jsp", req, resp);
+                break;
+        }
+    }
+
+    private void add(String url, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArticleService.add(req, resp);
+        ServletUtils.redirect(url, req, resp);
+    }
+
+    private void edit(String url, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String title = req.getParameter("TinyDes");
+        System.out.println("Add new:" + title);
+        ServletUtils.redirect(url, req, resp);
     }
 }
